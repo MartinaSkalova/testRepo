b9bbd735561fabda0890dfbbe053512579d72a5e
MartinaSkalova <mskalova@gmail.com>
2022-06-09 15:35:46 -0500

    new code

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
 }
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println("Hello, World!")
-	fmt.Printf("Now you are %d people's problem.\n", math.MaxInt)
 	fmt.Printf("34 + 65 is %d \n", add(34, 65))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
